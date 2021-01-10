67c51211b5a5f338a6efa709912d8c7cb7802e31
Andreas Goulas <goulasand@gmail.com>
2021-01-10 22:21:58 +0200

    Change PaymentMethods URL and optimize code

--- shotgun.py
+++ shotgun.py
@@ -1221,37 +1221,33 @@ def EditUserProfile(username):
                                    message="User does not exist or unauthorized edit was attempted.")
 
 
-@app.route('/user/<string:username>/paymentmethods', methods=['GET', 'POST'])
-def PaymentMethods(username):
+@app.route('/user/paymentmethods', methods=['GET', 'POST'])
+def PaymentMethods():
     if request.method == 'GET':
-
         # Check if user logged in
         if 'username' not in session:
             return redirect(url_for('Login'))
 
-        # Check if the provided username belongs to the currently logged in user
-        if session['username'] == username:
+        # Get payment methods
+        username = session['username']
+        response = requests.get(url_for('UserPaymentInfo', username=username, _external=True))
+        paymentMethods = response.json()
 
-            # Get payment methods
-            paymentMethods = requests.get(url_for('UserPaymentInfo', username=username, _external=True))
-            # Check for errors
-            # TODO: Possibly handle this more gracefully in the future
-            if ('error' in paymentMethods.json()) and (paymentMethods.json()['error'] != 'User has no payment info in the database'):
-                return render_template("systemMessage.html", messageTitle="An error occurred",
-                                       message=paymentMethods.json()['error'])
+        # Check for errors
+        # TODO: Possibly handle this more gracefully in the future
+        if ('error' in paymentMethods) and (paymentMethods['error'] != 'User has no payment info in the database'):
+            return render_template("systemMessage.html", messageTitle="An error occurred",
+                                   message=paymentMethods.json()['error'])
 
-            creditCards = paymentMethods.json()['credit_cards']
+        creditCards = paymentMethods['credit_cards']
+        paypalAccounts = paymentMethods['paypal_accounts']
 
-            # Censor Credit Card Numbers
-            for card in creditCards:
-                card['number'] = 3 * '**** ' + card['number'][-4:]
+        # Censor Credit Card Numbers
+        for card in creditCards:
+            card['number'] = 3 * '**** ' + card['number'][-4:]
 
-            # Render page
-            return render_template("paymentMethods.html", creditCards=creditCards,
-                                   paypalAccounts=paymentMethods.json()['paypal_accounts'])
-        else:
-            return render_template("systemMessage.html", messageTitle="Unauthorized Access",
-                                   message="You tried to access another user's payment methods.")
+        # Render page
+        return render_template('paymentMethods.html', creditCards=creditCards, paypalAccounts=paypalAccounts)
 
 
 @app.route('/user/<string:username>/set_primary_pm', methods=['POST'])
@@ -1273,7 +1269,7 @@ def UserSetPrimary(username):
                 return render_template("systemMessage.html", messageTitle="Error",
                                        message="An error occurred while setting the primary payment method.")
 
-            return redirect(url_for('PaymentMethods', username=session['username']))
+            return redirect(url_for('PaymentMethods'))
 
         else:
             return render_template("systemMessage.html", messageTitle="Unauthorized Access",
