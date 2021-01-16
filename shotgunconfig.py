7c6cf0c0c68c340076e40cda9fdbd6d9739e1a31
kostino <triaridis@ece.auth.gr>
2021-01-17 01:06:49 +0200

    Fix config bug KOSTINO STUPID

--- shotgunconfig.py
+++ shotgunconfig.py
@@ -6,9 +6,12 @@ class ShotgunConfig:
         c = configparser.ConfigParser()
         c.read(path)
         for item in c['general']['list'].split(','):
+            print(item)
             category = item.split(':')[0]
             var = item.split(':')[1]
-            setattr(self, var, c[category][var])
+            print(category)
+            print(var)
+            setattr(self, var, c[category][var].split(','))
         for category in c.sections():
             for item in c[category]:
                 if str(category)+':'+str(item) not in c['general']['list'] and item != 'list':
