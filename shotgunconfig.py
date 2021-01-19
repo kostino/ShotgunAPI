b8fa1c22d7069e82928be397bf0a9628898049c0
kostino <triaridis@ece.auth.gr>
2021-01-19 15:42:41 +0200

    Remove dbg prints

--- shotgunconfig.py
+++ shotgunconfig.py
@@ -6,11 +6,8 @@ class ShotgunConfig:
         c = configparser.ConfigParser()
         c.read(path)
         for item in c['general']['list'].split(','):
-            print(item)
             category = item.split(':')[0]
             var = item.split(':')[1]
-            print(category)
-            print(var)
             setattr(self, var, c[category][var].split(','))
         for category in c.sections():
             for item in c[category]:
